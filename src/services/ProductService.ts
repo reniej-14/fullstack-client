069d5537d9fd1ad3c8ee56120d6c087581955212
reniej-14 <jeinerclash@gmail.com>
2025-10-06 23:26:37 -0600

    Solucionando problemas con CORS

--- src/services/ProductService.ts
+++ src/services/ProductService.ts
@@ -1,8 +1,29 @@
+import { DraftProductSchema } from "../types"
+import axios from "axios"
+import { safeParse } from "valibot"
 
 type ProductData = {
     [k: string]: FormDataEntryValue
 }
 
 export async function addProduct(data : ProductData) {
-    console.log(data)
+    try {
+        const result = safeParse(DraftProductSchema, {
+            name: data.name,
+            price: +data.price
+        })
+        if (result.success) {
+            const url = `${import.meta.env.VITE_API_URL}/api/products`
+            const { data } = await axios.post(url, {
+                name: result.output.name,
+                price: result.output.price
+            })
+            console.log(data)
+        } else {
+            throw new Error('Datos no válidos')
+        }
+        
+    } catch (error) {
+        console.log(error)
+    }
 }
